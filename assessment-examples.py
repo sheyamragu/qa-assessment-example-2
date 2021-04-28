259a6f9c850155041ebb391e77a3c997c83ec48b
Ollie Nichols <ollienichols22@gmail.com>
2021-04-28 08:14:28 +0000

    fixed whitespace

--- assessment-examples.py
+++ assessment-examples.py
@@ -1,45 +1,45 @@
-	# <QUESTION 1>
+    # <QUESTION 1>
 
-	# Given a word and a string of characters, return the word with all of the given characters
+    # Given a word and a string of characters, return the word with all of the given characters
     # replaced with underscores
 
     # This should be case sensitive
 
-	# <EXAMPLES>
+    # <EXAMPLES>
 
-	# one("hello world", "aeiou") → "h_ll_ w_rld"
+    # one("hello world", "aeiou") → "h_ll_ w_rld"
     # one("didgeridoo", "do") → "_i_geri___"
-	# one("punctation, or something?", " ,?") → "punctuation__or_something_"
+    # one("punctation, or something?", " ,?") → "punctuation__or_something_"
 
 def one(word, chars):
     pass
 
-	# <QUESTION 2>
+    # <QUESTION 2>
 
-	# Given an integer - representing total seconds - return a tuple of integers (of length 4) representing 
+    # Given an integer - representing total seconds - return a tuple of integers (of length 4) representing 
     # days, hours, minutes, and seconds
 
-	# <EXAMPLES>
+    # <EXAMPLES>
 
-	# two(270) → (0, 0, 4, 30)
+    # two(270) → (0, 0, 4, 30)
     # two(3600) → (0, 1, 0, 0)
-	# two(86400) → (1, 0, 0, 0)
+    # two(86400) → (1, 0, 0, 0)
 
-	# <HINT>
+    # <HINT>
 
     # There are 86,400 seconds in a day, and 3600 seconds in an hour
 
 def two(total_seconds):
     pass
 
-	# <QUESTION 3>
+    # <QUESTION 3>
 
-	# Given a dictionary mapping keys to values, return a new dictionary mapping the values
+    # Given a dictionary mapping keys to values, return a new dictionary mapping the values
     # to their corresponding keys
 
-	# <EXAMPLES>
+    # <EXAMPLES>
 
-	# three({'hello':'hola', 'thank you':'gracias'}) → {'hola':'hello', 'gracis':'thank you'}
+    # three({'hello':'hola', 'thank you':'gracias'}) → {'hola':'hello', 'gracis':'thank you'}
     # three({101:'Optimisation', 102:'Partial ODEs'}) → {'Optimisation':101, 'Partial ODEs':102}
 
     # <HINT>
@@ -51,14 +51,14 @@ def three(dictionary):
 
     # <QUESTION 4>
 
-	# Given an integer, return the largest of the numbers this integer is divisible by
+    # Given an integer, return the largest of the numbers this integer is divisible by
     # excluding itself
 
     # This should also work for negative numbers
 
-	# <EXAMPLES>
+    # <EXAMPLES>
 
-	# four(10) → 5
+    # four(10) → 5
     # four(24) → 12
     # four(7) → 1
     # four(-10) → 5
@@ -68,11 +68,11 @@ def four(number):
 
     # <QUESTION 5>
 
-	# Given an string of characters, return the character with the lowest ASCII value
+    # Given an string of characters, return the character with the lowest ASCII value
 
-	# <EXAMPLES>
+    # <EXAMPLES>
 
-	# five('abcdef') → 'a'
+    # five('abcdef') → 'a'
     # four('LoremIpsum') → 'I'
     # four('hello world!') → ' '
 
